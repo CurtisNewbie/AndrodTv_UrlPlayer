ed55746bc81fcab6ba950a900f0fd42ba873a68a
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-13 20:11:36 +0800

    Hide support action bar & add MediaController

--- urlPlayer/app/src/main/java/com/curtisnewbie/activities/MediaPlayerActivity.java
+++ urlPlayer/app/src/main/java/com/curtisnewbie/activities/MediaPlayerActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MediaPlayerActivity extends AppCompatActivity {
@@ -15,8 +16,11 @@ public class MediaPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
+        getSupportActionBar().hide();
+
         url = getIntent().getStringExtra(MediaListAdapter.URL_TAG);
         videoView = findViewById(R.id.videoView);
+        videoView.setMediaController(new MediaController(this));
         videoView.setVideoURI(Uri.parse(url));
         videoView.start();
     }
