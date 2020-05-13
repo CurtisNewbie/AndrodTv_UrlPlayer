1780a87ae67061c7c49d4392de08b522d3fcf08f
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-13 13:53:04 +0800

    Update main activity UI

--- urlPlayer/app/src/main/java/com/curtisnewbie/activities/MainActivity.java
+++ urlPlayer/app/src/main/java/com/curtisnewbie/activities/MainActivity.java
@@ -3,18 +3,22 @@ package com.curtisnewbie.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.curtisnewbie.services.App;
 import com.curtisnewbie.services.HttpService;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
-    HttpService httpService;
+    protected HttpService httpService;
+
+    protected EditText serverIpEt = findViewById(R.id.serverIpEt);
+    protected Button connectServerBtn = findViewById(R.id.connectServerBtn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +26,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        httpService.init("192.168.1.12");
-        new Thread(new Runnable() {
+        this.connectServerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                List<String> list = httpService.getMediaList();
-                for (String s : list) {
-                    System.out.println("URLPlayer " + s);
+            public void onClick(View v) {
+                String ip = serverIpEt.getText().toString().trim();
+                if (!ip.isEmpty()) {
+                    httpService.init(ip);
                 }
             }
-        }).start();
+        });
     }
 }
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                List<String> list = httpService.getMediaList();
+//                for (String s : list) {
+//                    System.out.println("URLPlayer " + s);
+//                }
+//            }
+//        }).start();
